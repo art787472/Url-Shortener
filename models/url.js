569d6cea9089b0b341eaa9b7d65d5e6c71f78013
Chen Yi-Cheng <art787472@gmail.com>
2022-06-15 17:41:47 +0800

    feat: add routers

---
+++ models/url.js
@@ -0,0 +1,15 @@
+
+const { default: mongoose } = require('mongoose')
+const db = require('./config/monogoose')
+const Schema = mongoose.Schema
+const urlSchema = new Schema ({
+  url: {
+    type: String,
+    required: true
+  },
+  token: {
+    type: String
+  }
+})
+
+module.exports = mongoose.model('Url', urlSchema)
