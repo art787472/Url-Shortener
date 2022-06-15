9860bea90bc485041ab0d1065a35a41973c58289
Chen Yi-Cheng <art787472@gmail.com>
2022-06-15 17:22:54 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const port = process.env.PORT || 3030
+app = express()
+
+
+app.get('/', (req, res) => {
+  res.write("Hello")
+  res.end()
+})
+
+
+app.listen(port, () => {
+  console.log(`app is listening on ${port}`)
+})
+
+
